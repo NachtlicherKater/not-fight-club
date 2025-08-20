cf2d2205fac244864c4fc669768e8c3f2ff01d78
Dmitry Salashchenko <ho4houkot39@gmail.com>
2025-08-21 02:49:59 +0300

    fix: save image of pokemon

--- scripts/user-info.js
+++ scripts/user-info.js
@@ -1,7 +1,7 @@
 const nickInput = document.getElementById('nick-input');
 const confirmButton = document.getElementById('save-name');
 const playerName = document.querySelectorAll('.player-name'); // all player-names
-const avatarContainer = document.getElementById('reg-form-container-img');
+const avatarContainer = document.querySelectorAll('.player-pokemon-picture');
 
 
 const savedNick = localStorage.getItem("nickname");
@@ -19,31 +19,38 @@ if (savedSex) {
 }
 if (savedPokemon) {
   document.querySelector(`input[name="selected-pokemon"][value="${savedPokemon}"]`).checked = true; // selected pokemon
+  avatarContainer.forEach(container => {
+    container.innerHTML = `<img src="./assets/images/characters/${savedPokemon}/static.png" alt="This is ${savedPokemon} !!!">`;
+  });
 }
 
 
 
-confirmButton.addEventListener("click", () => {
-  const username = nickInput.value;           
-  localStorage.setItem("nickname", username); 
-  playerName.forEach(element => {
-    element.textContent = username; // set nicknames to all classes 
-  });        
+
+nickInput.addEventListener('input', () => {
+    const username = nickInput.value;
+    localStorage.setItem("nickname", username);
+    playerName.forEach(elemnent => elemnent.textContent = username); 
+});
+
+confirmButton.addEventListener("click", () => {     
   const selectedSex = document.querySelector('input[name="sex"]:checked').value;
   localStorage.setItem("sex", selectedSex);
   const selectedPokemon = document.querySelector('input[name="selected-pokemon"]:checked').value;
   localStorage.setItem("selectedPokemon", selectedPokemon);
-
-  avatarContainer.innerHTML = `<img src="./assets/images/characters/${selectedPokemon}/static.png" alt="${selectedPokemon}">`;
+  avatarContainer.forEach(container => {
+  container.innerHTML = `<img src="./assets/images/characters/${selectedPokemon}/static.png" alt="This is ${selectedPokemon} !!!">`;
 });
-
+ });
 
 const choosePokemon = document.querySelectorAll('input[name="selected-pokemon"]');
 choosePokemon.forEach(choose => {
   choose.addEventListener('change', () => {
     if (choose.checked) {
       const selectedPokemon = choose.value;
-      avatarContainer.innerHTML = `<img src="./assets/images/characters/${selectedPokemon}/static.png" alt="This is ${selectedPokemon} !!!">`;
-    }
+      avatarContainer.forEach(container => {
+        container.innerHTML = `<img src="./assets/images/characters/${selectedPokemon}/static.png" alt="This is ${selectedPokemon} !!!">`;
+      })
+    ;}
   });
 });
